d56d752dbd9ca205909d8d35790da6327f9eb2ca
Shalini-Budhi <shalinibudhi16@gmail.com>
2026-05-07 16:11:27 +0530

    New Changes on flight component

--- Frontend/my-react-app/src/main.jsx
+++ Frontend/my-react-app/src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import TravelProvider from "./Context/TravelContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <TravelProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TravelProvider>
+  </React.StrictMode>,
 );
